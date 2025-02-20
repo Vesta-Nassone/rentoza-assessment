737793eb93e4e95b8c3f1a2f49e40da4b4698995
Vesta-Nassone <vestanassone@gmail.com>
2025-02-20 23:34:26 +0200

    Implement the function to fetch product by id from API.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -11,6 +11,20 @@ export const fetchAllProducts = async (): Promise<Product[]> => {
         throw new Error('Failed to fetch products');
     }
     const responseJson = await response.json();
-    console.log(responseJson);
+    console.log("All products response: ", responseJson);
+    return responseJson;
+};
+
+
+// Fetch product by id from fakestoreapi.
+// Throw appropriate error on failure.
+// Return json response.
+export const fetchProductById = async (id: number): Promise<Product> => {
+    const response = await fetch(`${BASE_URL}/products/${id}`);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch product with id ${id}`);
+    }
+    const responseJson = await response.json();
+    console.log(`Product ID:: ${id} \nResponse:: ${responseJson}`);
     return responseJson;
 };
