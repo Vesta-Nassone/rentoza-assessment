9833a0eed3f3ff34b66139ca773e995bbc02568e
Vesta-Nassone <vestanassone@gmail.com>
2025-02-21 14:09:01 +0200

    Render the Product list on the Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import ProductList from '../components/products/ProductList';
 
 const HomePage: FC = () => {
     return (
         <div>
             <h1 className="text-3xl font-bold text-gray-800 mb-8">Featured Products</h1>
+            <ProductList />
         </div>
     );
 };
