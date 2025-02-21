d89f5ccb7bcf068cfa2df9ba8c0b17fd18472e98
Vesta-Nassone <vestanassone@gmail.com>
2025-02-21 14:25:27 +0200

    Display the products in the cart.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import Cart from '../components/products/Cart';
 
 const CartPage: FC = () => {
     return (
         <div className="pt-8 mt-8">
             <h1 className="text-3xl font-bold text-gray-800 mb-8 pt-8">Your Shopping Cart</h1>
+            <Cart/>
         </div>
     );
 };
