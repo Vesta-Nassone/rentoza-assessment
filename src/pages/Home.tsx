e22ff32e76d55839f7b62e9145a9511b3a81d4d4
Vesta-Nassone <vestanassone@gmail.com>
2025-02-21 13:42:34 +0200

    Add the Home page component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const HomePage: FC = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-gray-800 mb-8">Featured Products</h1>
+        </div>
+    );
+};
+
+export default HomePage;
