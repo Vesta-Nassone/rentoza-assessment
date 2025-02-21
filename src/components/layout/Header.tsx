d93914e96f7c8e2e53dc800540b7f08fe32d9f28
Vesta-Nassone <vestanassone@gmail.com>
2025-02-21 15:41:51 +0200

    Remove the products link.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,10 +15,6 @@ const Header: FC = () => {
                 </Link>
 
                 <nav className="flex items-center">
-                    <Link to="/" className="mr-6 hover:text-indigo-200 transition-colors">
-                        Products
-                    </Link>
-
                     <Link to="/cart" className="relative group">
                         <div className="p-2 bg-transparent text-black rounded-full hover:bg-gray-200 hover:rounded-full hover:shadow-lg transition-colors">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
