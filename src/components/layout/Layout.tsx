480cee566d48f1480e1c87480a797a99a3a0b840
Vesta-Nassone <vestanassone@gmail.com>
2025-02-21 14:26:38 +0200

    Add the main content section.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from './Header';
 
 const Layout: FC = () => {
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col">
             <Header />
+            <main className="flex-grow container mx-auto px-4 py-8">
+                <Outlet />
+            </main>
         </div>
     );
 };
