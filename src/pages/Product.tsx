5cb3aa931ed437e9f6a4f1688129f7bb9978e5b7
Vesta-Nassone <vestanassone@gmail.com>
2025-02-21 16:02:56 +0200

    Add cursor pointer events.

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -104,7 +104,7 @@ const ProductPage: FC = () => {
                     <div className="flex space-x-4">
                         <button type="button"
                             onClick={handleAddToCart}
-                            className="flex-grow py-3 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition-colors flex items-center justify-center"
+                            className="flex-grow py-3 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition-colors flex items-center justify-center cursor-pointer"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
@@ -114,7 +114,7 @@ const ProductPage: FC = () => {
 
                         <button type="button"
                             onClick={() => navigate('/')}
-                            className="px-6 py-3 border border-gray-300 text-gray-700 rounded-md hover:bg-gray-100 transition-colors"
+                            className="px-6 py-3 border border-gray-300 text-gray-700 rounded-md hover:bg-gray-100 transition-colors cursor-pointer"
                         >
                             Back
                         </button>
